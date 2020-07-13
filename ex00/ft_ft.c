f6028ef5c5f2cc3c7047cfef9b69be2ae60c067a
Mecayin Mia <memia@c4r2s5.wethinkcode.co.za>
2020-07-13 14:02:40 +0200

    ex00 done

--- ex00/ft_ft.c
+++ ex00/ft_ft.c
@@ -1,13 +1,2 @@
-#include <stdio.h>
-
-int main(){
-    int x = 42;
-    printf("x is %d\n", x);
-
-    int *aptr = &x;
-
-    printf("aptr is %d\n" , aptr);
-
-    printf("x is %d\n", *aptr);
-
-}
+void ft_ft(int *nbr)
+	*nbr=42;
